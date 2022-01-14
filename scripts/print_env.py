367867ed7133380cc166071ce3ee797ed4b67325
Andrea Hrelja <andhrelja@hotmail.com>
2022-01-14 15:52:58 +0100

    Script updates

--- scripts/print_env.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-if __name__ == '__main__':
-    print(json.dumps({
-        key: dict(os.environ)[key]
-        for key in sorted(dict(os.environ).keys())
-    }, indent=2))
