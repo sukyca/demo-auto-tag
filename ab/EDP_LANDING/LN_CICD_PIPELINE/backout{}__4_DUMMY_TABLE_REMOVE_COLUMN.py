d873aab98f9feb65d77f47848ceee8b90b94cb96
Andrea Hrelja <andhrelja@hotmail.com>
2022-02-08 10:12:44 +0100

    Minor update

--- ab/EDP_LANDING/LN_CICD_PIPELINE/backout{}__4_DUMMY_TABLE_REMOVE_COLUMN.py
+++ ab/EDP_LANDING/LN_CICD_PIPELINE/backout{}__4_DUMMY_TABLE_REMOVE_COLUMN.py
@@ -1,8 +1,13 @@
 from scripts.backout_scripts import backout_functions
 
-backout_functions.undo_add_column(
-    database='EDP_LANDING', 
-    schema='LN_CICD_PIPELINE', 
-    table_name='DUMMY_TABLE', 
-    column_name='TEST_COLUMN'
+database = 'EDP_LANDING', 
+schema = 'LN_CICD_PIPELINE', 
+table_name = 'DUMMY_TABLE', 
+column_name = 'TEST_COLUMN'
+
+
+backout_functions.undo_drop_table(
+    database=database,
+    schema=schema,
+    table_name=table_name
 )
