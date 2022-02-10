fa5e48e2140cee86f96c3378681a1b2e6e3ad94f
Andrea Hrelja <andhrelja@hotmail.com>
2022-02-10 13:57:15 +0100

    Update logging

--- scripts/make_flyway.py
+++ scripts/make_flyway.py
@@ -180,7 +180,7 @@ def generate_flyway_commands(scripts_to_deploy, command):
             location = 'filesystem://{}'.format(os.path.join(config.FLYWAY_FILESYSTEM_DIR, db, schema_name))
             config_file = os.path.join(config.FLYWAY_CONFIG_DIR, '{}.{}.config'.format(db, schema_name))
             output_file = os.path.join(config.FLYWAY_OUTPUT_DIR, command, '{}.{}.json'.format(db, schema_name))
-            cmd = 'flyway -locations="{}" -configFiles="{}" -schemas={} -outputFile="{}" -outputType="json" {} &> "{}"'.format(
+            cmd = 'flyway -locations="{}" -configFiles="{}" -schemas={} -outputFile="{}" -outputType="json" {} 1> /dev/null'.format(
                 location, config_file, schema_name, output_file, command, output_file
             )
             migrate_cmds.append(cmd)
