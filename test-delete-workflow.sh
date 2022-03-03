00d5096fdb12723c517162224fc3245cd97ee7de
sukyca <antonio.suljic1@gmail.com>
2022-03-03 13:57:54 +0100

    test for delete workflow

---
+++ test-delete-workflow.sh
@@ -0,0 +1,102 @@
+# test delete workflow 
+
+# valid test 1-> create test-delete-workflow-branch, create a pull request to development
+# allow merge to development, try to delete test-delete-workflow-branch - if test-delete-workflow-branch gets
+# recreated then test is successfull
+
+# valid test 2 -> create pull request to production
+# allow merge to production, after merge is complete try to delete test-delete-workflow-branch
+# if test-delete-workflow-branch is deleted the test is successfull
+
+git checkout production &> /dev/null
+git pull --all &> /dev/null
+git checkout -b test-delete-workflow-branch &> /dev/null
+echo -e '\n' >> README.md &> /dev/null
+git add . &> /dev/null
+git commit -m "test-delete-workflow-branch" &> /dev/null
+git push origin test-delete-workflow-branch &> /dev/null
+sleep 30
+# waiting for create_branch workflow to complete
+gh pr create -B development -H test-delete-workflow-branch --fill &> /dev/null
+PR_ID=$(gh pr list --limit 1 --json number | tr -dc '0-9')
+sleep 10
+RUN_ID=$(gh run list --limit 1 --json databaseId | tr -dc '0-9')
+# open_workflow.yml starts - wait for check completion
+gh run watch $RUN_ID --exit-status &> /dev/null
+
+if [ $? = 0 ]
+then 
+    echo "test-delete-workflow-branch: OPEN-PR (development) SUCCESS"
+    gh pr merge $PR_ID --squash
+    sleep 10
+    RUN_ID=$(gh run list --limit 1 --json databaseId | tr -dc '0-9')
+    # close_workflow.yml starts - wait for check completion
+    gh run watch $RUN_ID --exit-status &> /dev/null
+
+    if [ $? = 0 ]
+    then 
+        echo "test-delete-workflow-branch: CLOSE-PR (development) SUCCESS"
+    else
+        echo "test-delete-workflow-branch: CLOSE-PR (development) FAIL"
+        exit 1
+    fi
+else
+    echo "test-delete-workflow-branch: OPEN-PR (development) FAILURE"
+    exit 1
+fi
+
+git branch -D test-delete-workflow-branch &> /dev/null
+git push origin --delete test-delete-workflow-branch &> /dev/null
+sleep 10
+RUN_ID=$(gh run list --limit 1 --json databaseId | tr -dc '0-9')
+gh run watch $RUN_ID --exit-status &> /dev/null
+
+if [ $? = 1 ]
+then 
+    echo "test-delete-workflow-branch: Delete workflow recreated branch - Workflow is working properly"
+else
+    echo "test-delete-workflow-branch: Delete workflow deleted branch - Workflow is not working properly"
+    exit 1
+fi
+
+gh pr create -B production -H test-delete-workflow-branch --fill &> /dev/null
+PR_ID=$(gh pr list --limit 1 --json number | tr -dc '0-9')
+sleep 10
+RUN_ID=$(gh run list --limit 1 --json databaseId | tr -dc '0-9')
+# open_workflow.yml starts - wait for check completion
+gh run watch $RUN_ID --exit-status &> /dev/null
+
+if [ $? = 0 ]
+then 
+    echo "test-delete-workflow-branch: OPEN-PR (production) SUCCESS"
+    gh pr merge $PR_ID --squash
+    sleep 10
+    RUN_ID=$(gh run list --limit 1 --json databaseId | tr -dc '0-9')
+    # close_workflow.yml starts - wait for check completion
+    gh run watch $RUN_ID --exit-status &> /dev/null
+
+    if [ $? = 0 ]
+    then 
+        echo "test-delete-workflow-branch: CLOSE-PR (production) SUCCESS"
+    else
+        echo "test-delete-workflow-branch: CLOSE-PR (production) FAIL"
+        exit 1
+    fi
+else
+    echo "test-delete-workflow-branch: OPEN-PR (production) FAILURE"
+    exit 1
+fi
+
+git branch -D test-delete-workflow-branch &> /dev/null
+git push origin --delete test-delete-workflow-branch &> /dev/null
+sleep 10
+RUN_ID=$(gh run list --limit 1 --json databaseId | tr -dc '0-9')
+gh run watch $RUN_ID --exit-status &> /dev/null
+
+if [ $? = 0 ]
+then 
+    echo "test-delete-workflow-branch: Delete workflow deleted branch - Workflow is working properly"
+else
+    echo "test-delete-workflow-branch: Delete workflow recreated branch - Workflow is not working properly"
+    exit 1
+fi
