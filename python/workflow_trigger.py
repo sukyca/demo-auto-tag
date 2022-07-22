16664da0e09c7ed61ed133db645170c151355db5
sukyca <antonio.suljic1@gmail.com>
2022-07-22 10:13:02 +0200

    lower index

--- python/workflow_trigger.py
+++ python/workflow_trigger.py
@@ -26,7 +26,7 @@ def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
         }
       }
 
-      responsevalue=requests.post("https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
+      responsevalue=requests.post("https://api.github.com/repos/{OWNER}/{REPO}/dispatches",data=data,headers=headers)
       print("The respoinse message is ",responsevalue.content)
 
 trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision)
